52caab4d1006466ee4dbe5d072660f66dd48fabd
peschelf <peschelf@physik.hu-berlin.de>
2022-04-21 15:47:25 +0200

    generate run directory if not present

--- excitingworkflow/src/exciting_calculation.py
+++ excitingworkflow/src/exciting_calculation.py
@@ -64,8 +64,8 @@ class ExcitingCalculation(CalculationIO):
         from where the necessary files STATE.OUT and EFERMI.OUT are copied
         :param xs: optional xml xs info
         """
-        if not os.path.isdir(self.directory):
-            os.mkdir(self.directory)
+        # if not os.path.isdir(self.directory):
+        #     os.mkdir(self.directory)
         super().__init__(name, directory)
         self.path_to_species_files = None
         self.unique_species = None
